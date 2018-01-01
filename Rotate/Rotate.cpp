ef96566f80b8c023c57fa01b9b15d75c4cf0498a
Solemnjoker <wenxiongkjsm@163.com>
2018-01-01 22:56:13 +0800

    update

--- Rotate/Rotate.cpp
+++ Rotate/Rotate.cpp
@@ -22,8 +22,13 @@ int _tmain(int argc, _TCHAR* argv[])
     cl_kernel kernel = clm.CreateKernel(program,"rotate_kernel");
     cv::Mat img = cv::imread("test.jpg");
     cv::Mat img_result(img.size(),img.type());
+	float scale = 0.5;
+    cv::Mat img_resize(img.size()/2,img.type());;
+
     cl_mem raw = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, img.dataend - img.datastart, img.data, nullptr);
     cl_mem result = clCreateBuffer(context, CL_MEM_READ_WRITE , img.dataend - img.datastart, nullptr, nullptr);
+	cl_mem resize = clCreateBuffer(context, CL_MEM_READ_WRITE, img_resize.dataend - img_resize.datastart, nullptr , nullptr);
+
     if (raw && result)
     {
         float theta = M_PI*20.0/180.0;
@@ -38,9 +43,9 @@ int _tmain(int argc, _TCHAR* argv[])
         if (err == CL_SUCCESS)
         {
             size_t globalThreads[] = { img.cols, img.rows, 3 };
-            size_t localThreads[] = { 16, 16 };
+            size_t localThreads[] = { 16, 16,1 };
             cl_event ev;
-            err = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, globalThreads, localThreads, 0, NULL, &ev);
+            err = clEnqueueNDRangeKernel(command_queue, kernel, 3, NULL, globalThreads, localThreads, 0, NULL, &ev);
             auto status = clFlush(command_queue);
             clWaitForEvents(1, &ev);
 
@@ -61,6 +66,46 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         return 1;
     }
+
+    cl_kernel resize_ker = clm.CreateKernel(program,"scale_kernel");
+    if (raw && resize)
+    {
+        int err = clSetKernelArg(resize_ker, 0, sizeof(cl_mem), &result);
+        err |= clSetKernelArg(resize_ker, 1, sizeof(cl_mem), &resize);
+        err |= clSetKernelArg(resize_ker, 2, sizeof(int),  (void *)&img.cols);
+        err |= clSetKernelArg(resize_ker, 3, sizeof(int),  (void *)&img.rows);
+        err |= clSetKernelArg(resize_ker, 4, sizeof(int),  (void *)&img_resize.cols);
+        err |= clSetKernelArg(resize_ker, 5, sizeof(int),  (void *)&img_resize.rows);
+        err |= clSetKernelArg(resize_ker, 6, sizeof(float),  (void *)&scale);
+ 
+        if (err == CL_SUCCESS)
+        {
+            size_t globalThreads[] = { img_resize.cols, img_resize.rows, 3 };
+            size_t localThreads[] = { 16, 16,1 };
+            cl_event ev;
+            err = clEnqueueNDRangeKernel(command_queue, resize_ker, 3, NULL, globalThreads, localThreads, 0, NULL, &ev);
+            auto status = clFlush(command_queue);
+            clWaitForEvents(1, &ev);
+
+            if (err != CL_SUCCESS)
+            {
+                std::cerr << "Error queuing kernel for execution." << std::endl;
+                return 1;
+            }
+
+            // Read the output buffer back to the Host
+            err = clEnqueueReadBuffer(command_queue,resize, CL_TRUE,
+                0, img_resize.dataend - img_resize.datastart, img_resize.data,
+                0, NULL, NULL);
+            int a = 0;
+        }
+    }
+    else
+    {
+        return 1;
+    }
+
+
 
 	return 0;
 }
--- Rotate/rotate.cl
+++ Rotate/rotate.cl
@@ -12,3 +12,26 @@ __kernel void rotate_kernel(__global char* src_data,__global char* dest_data, in
         dest_data[(ypos*W+xpos)*3 + ic]= src_data[(iy*W+ix)*3 + ic];
     }
 }
+
+__kernel void scale_kernel(__global char* src_data,__global char* dst_data, int srcW, int srcH,int dstW,int dstH, float scale)
+{
+    const int ix = get_global_id(0);
+    const int iy = get_global_id(1);
+    const int ic = get_global_id(2);
+    const float srcX = ix/scale;
+    const float srcY = iy/scale;
+    int x0 = srcX;
+    int x1 = srcX + 1;
+    int y0 = srcY ;
+    int y1 = srcY + 1;
+    if(x0 >0 && x1 <srcW && y0 > 0 && y1 < srcH)
+    {
+        float ax = srcX - x1;
+        float ay = srcY - y1;
+        int line0 = y0*srcW;
+        int line1 = line0 + srcW;
+        dst_data[(ix+iy*dstW)*3 + ic] = ((ax + ay)*src_data[(line0 + x0) * 3 + ic] + (1-ax + ay)*src_data[(line0 + x1)*3+ic] 
+        + (ax + 1- ay)*src_data[(line1 + x0)*3 + ic] + (1-ax + 1- ay)*src_data[(line1 + x1)*3 + ic])/4;
+    }
+}
+
